43223133037813ffd1e23f875815358618fec278
heweiming <heweiming89@163.com>
2017-06-15 22:07:02 +0800

    删除静态文件

---
+++ merlion-console/src/main/java/org/together/merlion/controller/HelloWorldController.java
@@ -0,0 +1,11 @@
+package org.together.merlion.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloWorldController {
+
+	
+	
+	
+}
---
+++ merlion-mock/src/test/java/com/together/merlion/mock/TestHellWorld.java
@@ -0,0 +1,12 @@
+package com.together.merlion.mock;
+
+import org.testng.annotations.Test;
+
+public class TestHellWorld {
+	
+	@Test
+	public void f() {
+		
+	}
+	
+}
