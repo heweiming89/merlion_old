8b7f10639299346417eea521ba4b6980a6c7da95
heweiming <heweiming89@163.com>
2017-06-15 23:49:27 +0800

    activiti 基础文件提交

---
+++ merlion-bpm/src/main/java/org/together/merlion/BpmApplication.java
@@ -0,0 +1,12 @@
+package org.together.merlion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpmApplication.class, args);
+	}
+}
